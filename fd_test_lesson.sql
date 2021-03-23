1144fef2c0cd374bbab39709d265d64d8d377912
Pavel <elena71vd@gmail.com>
2021-03-23 17:00:27 +0200

    Add lesson 23.03

--- fd_test_lesson.sql
+++ fd_test_lesson.sql
@@ -31,4 +31,43 @@ CREATE TABLE "users_to_orders"(
 /*  Посчитать кол-во телефонов, которые были проданы  */
 SELECT sum(quantity)
 FROM phones_to_orders;
-/*  */
+/* Кол-во тел, которые есть на складе */
+SELECT sum(quantity)
+FROM phones;
+/* Средняя цена всех тел */
+SELECT avg(price)
+FROM phones;
+/* Средняя цена каждого бренда тел */
+SELECT avg(price),
+brand
+FROM phones
+GROUP BY brand;
+/* Стоимость всех телефонов в диапазоне */
+SELECT sum(price * quantity)
+FROM phones
+WHERE  price BETWEEN 10000 AND 20000;
+/* Кол-во моделей каждого бренда */
+SELECT count(model),
+brand
+FROM phones
+GROUP BY brand;
+/* Узнать каких brand тел осталось меньше всего */
+SELECT sum(quantity) AS "res",
+brand
+FROM phones
+GROUP BY brand
+ORDER BY "res" ASC;
+/* model */
+SELECT * FROM phones
+order by quantity ASC;
+/* */
+SELECT *,
+ extract('year' from age ("birthday")) AS "age"
+ FROM users
+ORDER BY "age" ASC, "firstName" DESC;
+/* */
+SELECT sum(quantity), 
+brand 
+FROM phones
+GROUP BY brand
+HAVING sum(quantity)>77000;
